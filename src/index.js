149d25f71c321cfd190689ad45f1f5d102eb382b
tommie watts <tommie@sureapp.com>
2020-10-24 14:45:21 -0700

    k

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import printFromPackage from "./printFromPackage";
 
-import print from "./printFromPackage";
-import printName from "./printMyName";
-import testing from "./services/testing";
-
-export default {
-  printFromPackage: print,
-  printMyName: printName,
-  testing: testing,
-};
+export * from ".printFromPackage";
