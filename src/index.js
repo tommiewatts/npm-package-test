902319d354d2eced00c002a27f75f5447c53f34f
tommie watts <tommie@sureapp.com>
2020-10-24 14:50:31 -0700

    k

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import printFromPackage from "./printFromPackage";
 import printName from "./printMyName";
 import testing from "./services/testing";
 
 export default {
+  newPrint: printFromPackage,
   printMyName: printName,
   testing: testing,
 };
--- src/printFromPackage.js
+++ src/printFromPackage.js
@@ -1,9 +1,11 @@
 import times from "lodash/times";
 
 export default function printFromPackage() {
-  times(10, () =>
-    console.log(
-      "SURE-AUTO-INSURANCE: Hello from package once more, should not work like this right?"
-    )
-  );
+  const test = [];
+  for (let i = 0; i < 100; i++) {
+    console.log(i);
+    test.push(`item${i}`);
+  }
+
+  console.log(test);
 }
